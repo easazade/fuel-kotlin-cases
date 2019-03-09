76e0be6d192ae2adf179f39a242bd8b31ecd8a21
alireza <easazade@gmail.com>
2019-03-09 12:51:10 +0330

    first example

--- app/src/main/java/ir/easazade/fuelkotlin/MainActivity.kt
+++ app/src/main/java/ir/easazade/fuelkotlin/MainActivity.kt
@@ -2,11 +2,22 @@ package ir.easazade.fuelkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.kittinunf.fuel.httpGet
+import kotlinx.android.synthetic.main.activity_main.sendRequestBtn
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    sendRequestBtn.setOnClickListener {
+      "https://httpbin.org/get"
+        .httpGet()
+        .responseString { request, response, result ->
+          println(request)
+          println(response)
+          println(result)
+        }
+    }
   }
 }
